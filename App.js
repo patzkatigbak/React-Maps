39664fcff81c4b6ad9a4671c2ed6fff0089e1ad8
patzkatigbak <patzkatigbak@gmail.com>
2022-09-29 22:47:39 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState }from 'react';
+import {Text, Button, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+
+
 
 export default function App() {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 43.7949631,
+    longitude: -79.3596231,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        region={mapRegion}>
+
+         <Marker coordinate={mapRegion} title='My City' />
+
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  defaultText:{
+  fontSize: 20, 
+  fontFamily:'Times New Roman'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+
 });
